f7a02e058bf092df9f20b286e7972a17556833b9
Geza Nagy <geza.nagy@attrecto.com>
2023-09-18 13:26:51 +0200

    Session 01

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const updateCounter = (increase: boolean) => {
+    setCounter((currentValue) => {
+      return increase ? currentValue + 1 : currentValue - 1;
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
       </header>
+      <div className="container d-flex justify-content-center">
+        <div className="card bg-white shadow text-center p-4 m-4">
+          <h5>Counter: {counter}</h5>
+          <div className="d-flex justify-content-center flex-wrap gap-2">
+            <button
+              className="btn btn-primary"
+              onClick={() => updateCounter(true)}
+            >
+              Increase +
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => updateCounter(false)}
+            >
+              Decrease -
+            </button>
+            <button className="btn btn-danger" onClick={() => setCounter(0)}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
